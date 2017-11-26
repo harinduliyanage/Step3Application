791a2ab519179234a9ed5a399b30660c4180902a
harinduliyanage <harindu.sul@gmail.com>
2017-11-25 19:35:01 -0800

    add custom DAO interface User

---
+++ src/java/com/app2/dao/UserDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app2.dao;
+
+import com.app2.dao.model.User;
+
+/**
+ *
+ * @author Harindu.sul
+ */
+public interface UserDAO extends SuperDAO<User>{
+    
+}
