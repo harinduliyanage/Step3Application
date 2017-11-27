e415f4811b566e4a43e154f9d6a931b78685e4ce
harinduliyanage <harindu.sul@gmail.com>
2017-11-26 20:38:55 -0800

    add @Repository annotation

--- src/java/com/app2/dao/impl/UserDAOImpl.java
+++ src/java/com/app2/dao/impl/UserDAOImpl.java
@@ -13,11 +13,13 @@ import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Harindu.sul
  */
+@Repository
 public class UserDAOImpl implements UserDAO{
 
    @Autowired
